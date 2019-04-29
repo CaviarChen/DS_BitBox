9944457643363f4b4312275d7f238f5063ba39f8
Zijun Chen <c.zijunchen@gmail.com>
2019-04-29 12:25:22 +1000

    Updated MessageHandler template.

--- bitbox/src/main/java/unimelb/bitbox/MessageHandler.java
+++ bitbox/src/main/java/unimelb/bitbox/MessageHandler.java
@@ -14,40 +14,75 @@ public class MessageHandler {
         if (protocolType != null) {
             switch (protocolType) {
                 case INVALID_PROTOCOL:
+                    // log then disconnect (?)
                     break;
-                case CONNECTION_REFUSED:
-                    break;
-                case HANDSHAKE_REQUEST:
-                    break;
-                case HANDSHAKE_RESPONSE:
-                    break;
+
                 case FILE_CREATE_REQUEST:
-                    break;
-                case FILE_CREATE_RESPONSE:
+                    handleSpecificProtocol((Protocol.FileCreateRequest) protocol, conn);
                     break;
                 case FILE_DELETE_REQUEST:
-                    break;
-                case FILE_DELETE_RESPONSE:
+                    handleSpecificProtocol((Protocol.FileDeleteRequest) protocol, conn);
                     break;
                 case FILE_MODIFY_REQUEST:
-                    break;
-                case FILE_MODIFY_RESPONSE:
+                    handleSpecificProtocol((Protocol.FileModifyRequest) protocol, conn);
                     break;
                 case FILE_BYTES_REQUEST:
+                    handleSpecificProtocol((Protocol.FileBytesRequest) protocol, conn);
                     break;
                 case FILE_BYTES_RESPONSE:
+                    handleSpecificProtocol((Protocol.FileBytesResponse) protocol, conn);
                     break;
                 case DIRECTORY_CREATE_REQUEST:
-                    break;
-                case DIRECTORY_CREATE_RESPONSE:
+                    handleSpecificProtocol((Protocol.DirectoryCreateRequest) protocol, conn);
                     break;
                 case DIRECTORY_DELETE_REQUEST:
+                    handleSpecificProtocol((Protocol.DirectoryDeleteRequest) protocol, conn);
                     break;
+
+
+                // log then ignored
                 case DIRECTORY_DELETE_RESPONSE:
+                case DIRECTORY_CREATE_RESPONSE:
+                case FILE_CREATE_RESPONSE:
+                case FILE_DELETE_RESPONSE:
+                case FILE_MODIFY_RESPONSE:
                     break;
+
+                // invalid
+                case CONNECTION_REFUSED:
+                case HANDSHAKE_REQUEST:
+                case HANDSHAKE_RESPONSE:
                 default:
                     break;
             }
         }
+    }
+
+    private static void handleSpecificProtocol(Protocol.FileCreateRequest fileCreateRequest, Connection conn) {
+
+    }
+
+    private static void handleSpecificProtocol(Protocol.FileDeleteRequest fileDeleteRequest, Connection conn) {
+
+    }
+
+    private static void handleSpecificProtocol(Protocol.FileModifyRequest fileModifyRequest, Connection conn) {
+
+    }
+
+    private static void handleSpecificProtocol(Protocol.FileBytesRequest fileBytesRequest, Connection conn) {
+
+    }
+
+    private static void handleSpecificProtocol(Protocol.FileBytesResponse fileBytesResponse, Connection conn) {
+
+    }
+
+    private static void handleSpecificProtocol(Protocol.DirectoryCreateRequest directoryCreateRequest, Connection conn) {
+
+    }
+
+    private static void handleSpecificProtocol(Protocol.DirectoryDeleteRequest directoryDeleteRequest, Connection conn) {
+
     }
 }
