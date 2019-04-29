613160b3ffb44f3ffad8e22e5d98159474447235
PwzXxm <cepheidfov@gmail.com>
2019-04-29 10:58:53 +1000

    added messageHandler base

--- bitbox/src/main/java/unimelb/bitbox/MessageHandler.java
+++ bitbox/src/main/java/unimelb/bitbox/MessageHandler.java
@@ -1,7 +1,53 @@
 package unimelb.bitbox;
 
+
+import unimelb.bitbox.protocol.*;
+import unimelb.bitbox.protocol.ProtocolType.*;
+
+
 public class MessageHandler {
     public static void handleMessage(String message, Connection conn) {
 
+        Protocol protocol = ProtocolFactory.parseProtocol(message);
+
+        ProtocolType protocolType = ProtocolType.typeOfProtocol(protocol);
+        if (protocolType != null) {
+            switch (protocolType) {
+                case INVALID_PROTOCOL:
+                    break;
+                case CONNECTION_REFUSED:
+                    break;
+                case HANDSHAKE_REQUEST:
+                    break;
+                case HANDSHAKE_RESPONSE:
+                    break;
+                case FILE_CREATE_REQUEST:
+                    break;
+                case FILE_CREATE_RESPONSE:
+                    break;
+                case FILE_DELETE_REQUEST:
+                    break;
+                case FILE_DELETE_RESPONSE:
+                    break;
+                case FILE_MODIFY_REQUEST:
+                    break;
+                case FILE_MODIFY_RESPONSE:
+                    break;
+                case FILE_BYTES_REQUEST:
+                    break;
+                case FILE_BYTES_RESPONSE:
+                    break;
+                case DIRECTORY_CREATE_REQUEST:
+                    break;
+                case DIRECTORY_CREATE_RESPONSE:
+                    break;
+                case DIRECTORY_DELETE_REQUEST:
+                    break;
+                case DIRECTORY_DELETE_RESPONSE:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
