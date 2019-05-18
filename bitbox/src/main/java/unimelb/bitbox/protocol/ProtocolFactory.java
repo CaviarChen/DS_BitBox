4ea7cb6bfa1a611044966c07d7cd45270a869115
zijies1 <546641464@qq.com>
2019-05-18 16:56:14 +1000

    add request identifier and equal, hash functions for each protocol

--- bitbox/src/main/java/unimelb/bitbox/protocol/ProtocolFactory.java
+++ bitbox/src/main/java/unimelb/bitbox/protocol/ProtocolFactory.java
@@ -1,6 +1,7 @@
 package unimelb.bitbox.protocol;
 
 
+import unimelb.bitbox.Constants;
 import unimelb.bitbox.util.Document;
 
 import static unimelb.bitbox.Constants.PROTOCOL_FIELD_CMD;
@@ -52,6 +53,55 @@ public class ProtocolFactory {
         doc.append(PROTOCOL_FIELD_CMD, ProtocolType.typeOfProtocol(protocol).getKey());
         protocol.marshalToJson(doc);
         return doc.toJson();
+    }
+
+    public static Protocol identifyReq(Protocol protocol){
+        String command = ProtocolType.typeOfProtocol(protocol).getKey();
+        System.out.println(ProtocolType.typeOfProtocol(protocol).getKey());
+        if (protocol instanceof IRequest){
+            return protocol;
+        } else if (command.equals(Constants.PROTOCOL_TYPE_FILE_CREATE_RESPONSE)){
+            Protocol.FileCreateResponse response = (Protocol.FileCreateResponse)protocol;
+            Protocol.FileCreateRequest request= new Protocol.FileCreateRequest();
+            request.fileDes = response.fileDes;
+            return request;
+        } else if (command.equals(Constants.PROTOCOL_TYPE_FILE_DELETE_RESPONSE)){
+            Protocol.FileDeleteResponse response = (Protocol.FileDeleteResponse)protocol;
+            Protocol.FileDeleteRequest request= new Protocol.FileDeleteRequest();
+            request.fileDes = response.fileDes;
+            return request;
+        } else if (command.equals(Constants.PROTOCOL_TYPE_FILE_MODIFY_RESPONSE)){
+            Protocol.FileModifyResponse response = (Protocol.FileModifyResponse)protocol;
+            Protocol.FileModifyRequest request= new Protocol.FileModifyRequest();
+            request.fileDes = response.fileDes;
+            return request;
+        } else if (command.equals(Constants.PROTOCOL_TYPE_FILE_BYTES_RESPONSE)){
+            Protocol.FileBytesResponse response = (Protocol.FileBytesResponse)protocol;
+            Protocol.FileBytesRequest request= new Protocol.FileBytesRequest();
+            ProtocolField.FilePosition filePosition = new ProtocolField.FilePosition();
+            filePosition.len = response.fileContent.len;
+            filePosition.pos = response.fileContent.pos;
+            request.filePos = filePosition;
+            request.fileDes = response.fileDes;
+            return request;
+        } else if (command.equals(Constants.PROTOCOL_TYPE_DIRECTORY_CREATE_RESPONSE)){
+            Protocol.DirectoryCreateResponse response = (Protocol.DirectoryCreateResponse)protocol;
+            Protocol.DirectoryCreateRequest request= new Protocol.DirectoryCreateRequest();
+            request.dirPath = response.dirPath;
+            return request;
+        } else if (command.equals(Constants.PROTOCOL_TYPE_DIRECTORY_DELETE_RESPONSE)){
+            Protocol.DirectoryDeleteResponse response = (Protocol.DirectoryDeleteResponse)protocol;
+            Protocol.DirectoryDeleteRequest request= new Protocol.DirectoryDeleteRequest();
+            request.dirPath = response.dirPath;
+            return request;
+        } else {
+            // unhandled protocols:
+            //  - INVALID_PROTOCOL
+            //  - CONNECTION_REFUSED
+            //  - HANDSHAKE_REQUEST
+            //  - HANDSHAKE_RESPONSE
+            return protocol;
+        }
     }
 }
 
